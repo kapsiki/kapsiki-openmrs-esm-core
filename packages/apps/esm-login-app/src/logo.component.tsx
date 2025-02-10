e8e09f0a6dd8fc36244ef30df67781889ee26af1
fwagni <fredoly.wagni@kouete.com>
2025-02-10 12:14:41 +0100

    remove custom logo

--- packages/apps/esm-login-app/src/logo.component.tsx
+++ packages/apps/esm-login-app/src/logo.component.tsx
@@ -3,18 +3,20 @@ import { interpolateUrl, useConfig } from '@openmrs/esm-framework';
 import { type TFunction } from 'react-i18next';
 import { type ConfigSchema } from './config-schema';
 import styles from './login/login.scss';
-import EMRLogo from './assets/kapsiki-healthcare-logo.png';
 
 const Logo: React.FC<{ t: TFunction }> = ({ t }) => {
   const { logo } = useConfig<ConfigSchema>();
   return logo.src ? (
     <img
-      alt={logo.alt ? t(logo.alt) : t('openmrsLogo', 'EMR logo')}
+      alt={logo.alt ? t(logo.alt) : t('openmrsLogo', 'OpenMRS logo')}
       className={styles.logoImg}
       src={interpolateUrl(logo.src)}
     />
   ) : (
-    <img alt={logo.alt ? t(logo.alt) : t('openmrsLogo', 'EMR logo')} className={styles.logoImg} src={EMRLogo} />
+    <svg role="img" className={styles.logo}>
+      <title>{t('openmrsLogo', 'OpenMRS logo')}</title>
+      <use href="#omrs-logo-full-color"></use>
+    </svg>
   );
 };
 
