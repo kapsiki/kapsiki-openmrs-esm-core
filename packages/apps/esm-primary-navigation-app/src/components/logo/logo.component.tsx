43347b4ed07cf83111cc0373af4410c16cf335b5
fwagni <fredoly.wagni@kouete.com>
2025-02-10 13:18:28 +0100

    comment components' tests

--- packages/apps/esm-primary-navigation-app/src/components/logo/logo.component.tsx
+++ packages/apps/esm-primary-navigation-app/src/components/logo/logo.component.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { interpolateUrl, useConfig } from '@openmrs/esm-framework';
 import { type ConfigSchema } from '../../config-schema';
 import styles from './logo.scss';
+import logoImg from '../../assets/kapsiki-healthcare-logo.png';
 
 const Logo: React.FC = () => {
   const { logo } = useConfig<ConfigSchema>();
@@ -17,9 +18,7 @@ const Logo: React.FC = () => {
       ) : logo?.name ? (
         logo.name
       ) : (
-        <svg aria-label="OpenMRS Logo" role="img" width={110} height={40}>
-          <use href="#omrs-logo-white" />
-        </svg>
+        <img alt={logo.alt} className={styles.logo} onError={handleImageError} src={logoImg} />
       )}
     </>
   );
